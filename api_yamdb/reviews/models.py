b1c790e8f4a328c62e3dbcb90543ab3e5eaa8008
Vladimir Zelenin <jd60@mail.ru>
2022-06-04 02:45:59 +0500

    added Models Category Genge Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,19 +1,80 @@
 from django.core.validators import (
     MaxValueValidator, MinValueValidator
 )
+from datetime import date
 from django.db import models
 
 #from users.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=256,
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        ordering = ('-id',)
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=256,
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        ordering = ('-id',)
+
+
+class Title(models.Model):
+    name = models.TextField()
+    year = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(
+                limit_value=date.today().year,
+                message='Title\'s year can not be greater than current year',
+            )
+        ]
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+    )
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name[:15]
+
 
 class Review(models.Model):
     text = models.TextField()
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews'
+    # )
     score = models.PositiveSmallIntegerField(
         validators=[
             MinValueValidator(
@@ -50,11 +111,11 @@ class Review(models.Model):
 
 class Comment(models.Model):
     text = models.TextField()
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name='comments'
+    # )
     pub_date = models.DateTimeField(
         auto_now_add=True
     )
