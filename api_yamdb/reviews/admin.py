5fa5ccc3680a5512a3a12f6c94efd29430a01463
Evgeniy Mishin <dubreak@yandex.ru>
2022-06-04 09:49:53 +0300

    feat: Добавлена модель данных юзера, первые шаги для api регистрации и авторизации пользователей

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib import admin
 
 from .models import (
-    Comment, Review, Category, Genre, Title
+    Comment, Review, Category, Genre, Title, User
 )
 
 
@@ -24,7 +24,7 @@ class ReviewAdmin(admin.ModelAdmin):
         'pk',
         'text',
         'pub_date',
-        # 'author',
+        'author',
         'score',
         'title',
     )
@@ -32,6 +32,15 @@ class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-empty-'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'first_name', 'last_name', 'bio', 'role')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('role',)
+    list_editable = ('role',)
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Review, ReviewAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,7 +4,64 @@ from django.core.validators import (
 from datetime import date
 from django.db import models
 
-#from users.models import User
+from django.contrib.auth.models import AbstractUser
+
+USER_ROLES = [
+    ('user', 'user'),
+    ('moderator', 'moderator'),
+    ('admin', 'admin')
+]
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        'Логин пользователя',
+        max_length=200,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        'Адрес почты',
+        max_length=200,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    role = models.CharField(
+        'Роль пользователя',
+        max_length=200,
+        choices=USER_ROLES,
+        default='user'
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=True, )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_user(self):
+        return self.role == USER_ROLES[1][1]
+
+    @property
+    def is_moderator(self):
+        return self.role == USER_ROLES[2][1]
+
+    @property
+    def is_admin(self):
+        return self.role == USER_ROLES[3][1]
 
 
 class Category(models.Model):
@@ -70,11 +127,11 @@ class Title(models.Model):
 
 class Review(models.Model):
     text = models.TextField()
-    # author = models.ForeignKey(
-    #     User,
-    #     on_delete=models.CASCADE,
-    #     related_name='reviews'
-    # )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
     score = models.PositiveSmallIntegerField(
         validators=[
             MinValueValidator(
@@ -111,11 +168,11 @@ class Review(models.Model):
 
 class Comment(models.Model):
     text = models.TextField()
-    # author = models.ForeignKey(
-    #     User,
-    #     on_delete=models.CASCADE,
-    #     related_name='comments'
-    # )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     pub_date = models.DateTimeField(
         auto_now_add=True
     )
@@ -130,3 +187,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text[:15]
+
